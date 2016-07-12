eb0ac2b3fc8d32f89982d2193d196ba0ec204e8c
Diego Eann <deann21@punahou.edu>
2016-07-11 14:07:08 -1000

    Save cmd s lyke ur lif dedened on it xD

---
+++ cannon.js
@@ -0,0 +1,15 @@
+'use strict' 
+var Drone = require('drone'),
+    blocks = require('blocks'),
+    Events = require('events'),
+    Sounds = require('sounds');
+
+function cannonwarfare(event) {
+  for(var i = 0; i <= 1; i++;){
+    this.box(49,3,1,1)
+        .fwd(4)
+        .right(1)
+        .box(49,1,1,1)
+        
+   }
+};
