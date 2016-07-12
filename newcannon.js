f9176012066ef9f829d6563fd4afd0ef0cf69817
Diego Eann <deann21@punahou.edu>
2016-07-11 19:29:30 -1000

    roasted billard

---
+++ newcannon.js
@@ -0,0 +1,20 @@
+'use strict'
+var Drone = require('drone'),
+    blocks = require('blocks'),
+    Events = require('events'),
+    Sounds = require('sounds');
+// Fetch Libs, start blockstore
+var pos1,
+    pos2,
+    pos3,
+    pos4,
+    pos5,
+    pos6,
+    pos7,
+    pos8,
+    pos9,
+    pos10,
+    pos11;
+function newcannon(event){
+
+}
